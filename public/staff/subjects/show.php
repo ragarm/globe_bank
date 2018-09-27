ca71d905d59e49ad207b9b11909de1e583ff85b1
Heath Atchley <hatchley@yachiyoal.com>
2018-09-27 04:27:53 -0500

    Update

--- public/staff/subjects/show.php
+++ public/staff/subjects/show.php
@@ -4,19 +4,19 @@ require_once '../../../private/initialize.php';
 //echo $id;
 
 /*
- $id = $_GET['id'];
+$id = $_GET['id'];
 if (isset($_GET['id'])) {
-    $id = $_GET['id'];
+$id = $_GET['id'];
 } else {
-    $id = '1';
+$id = '1';
 }
  */
 
 // $id = isset($_GET['id']) ? $_GET['id'] : '1'; Easier Than above
 
- $id = $_GET['id'] ?? '1';    //PHP 7.0+
+$id = $_GET['id'] ?? '1'; //PHP 7.0+
 
- echo h($id);
+echo h($id);
 ?>
  <a href="show.php?name=<?php echo u('John Doe'); ?>">Link</a><br/>
  <a href="show.php?company=<?php echo u('Widgets&More'); ?>">Link</a><br/>
