4343030b0870b179099b60145e34fe62906ec8a5
ragarm <heath.atchley@outlook.com>
2018-09-26 19:55:39 -0500

    First Push

---
+++ private/shared/staff_header.php
@@ -0,0 +1,24 @@
+<?php
+    if (!isset($page_title)) {
+        $page_title = 'Staff Area';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GBI - <?php echo $page_title; ?></title>
+    <link rel="stylesheet" media="all" href="<?php echo url_for('/stylesheets/staff.css'); ?>"/>
+</head>
+<body>
+    <header>
+        <h1>BGI Staff Area</h1>
+    </header>
+    <navigation>
+        <ul>
+            <li><a href="<?php echo WWW_ROOT .  '/staff/index.php'; ?>">Menu</a></li>
+        </ul>
+    </navigation>
